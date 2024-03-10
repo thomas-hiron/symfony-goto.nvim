4880ee14a220cfbd32c840f7a0e4d99460db9ead
Thomas Hiron <thomashiron@citia.org>
2024-03-10 18:15:48 +0100

    Create webpack encore handler

---
+++ lua/symfony-goto/handler/encore.lua
@@ -0,0 +1,39 @@
+return function (config)
+
+  return function ()
+    -- Get whole line under cursor
+    local cursorLine = vim.api.nvim_get_current_line()
+
+    if not cursorLine:match("encore_entry") then
+      print("Not an encore_entry statement")
+
+      return
+    end
+
+    -- Get text inside quotes
+    local path = cursorLine:match("'.+'"):gsub("'", '')
+
+    local value
+    local replacement
+    local extension
+
+    if cursorLine:match('encore_entry_link') then
+      value, replacement = config.css_pattern:match("#(.-)#(.-)#")
+      extension = config.css_extension
+    else
+      value, replacement = config.js_pattern:match("#(.-)#(.-)#")
+      extension = config.js_extension
+    end
+
+    local realPath = path:gsub(value, replacement)..extension
+
+    -- Check if file exists if force_open is false
+    if not config.force_open and vim.fn.filereadable(realPath) == 0 then
+      print('File '..realPath..' doesn\'t exist')
+      return
+    end
+
+    -- Open the file
+    vim.cmd('edit '..realPath)
+  end
+end
