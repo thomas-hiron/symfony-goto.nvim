d4c8a74a73487811ec6602bc90998545af4c3d37
Thomas Hiron <thomashiron@citia.org>
2024-03-10 18:15:26 +0100

    Create plugin

---
+++ lua/symfony-goto/init.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+local config = require('symfony-goto.config')
+
+function M.setup(options)
+  options = vim.tbl_deep_extend("keep", options or {}, config)
+
+  vim.api.nvim_create_user_command(
+    'SymfonyGoto',
+    require('symfony-goto.handler.all')(options),
+    {desc = "Goto depending on the current line"}
+  )
+
+  if options.encore.enable then
+    vim.api.nvim_create_user_command(
+      'SymfonyGotoEncore',
+      require('symfony-goto.handler.encore')(options.encore),
+      {desc = "Goto to webpack encore asset"}
+    )
+  end
+end
+
+return M
